77c8c7c2a8f10d701b760bb49f8e28b010845f90
Shurooo <wl1191@nyu.edu>
2016-07-14 12:28:51 -0700

    Modify Day_Sampler

--- Day_Sampler.py
+++ Day_Sampler.py
@@ -1,39 +1,61 @@
 import os
+import time
 import random
+import multiprocessing
 from scipy.sparse import csr_matrix, vstack
 import Sparse_Matrix_IO as smio
 
 
-root = "/mnt/rips2/2016"
 num = 100000
+start = time.time()
 
-for month in [5, 6]:
-    month_str = str(month).rjust(2, "0")
-    for day in range(1,32):
-        day_str = str(day).rjust(2, "0")
-        res = []
-        count = 0
-        path_in_day = os.path.join(root, month_str, day_str)
-        flag = 0
-        for hour in range(0,24):
-            try:
-                hour_str = str(hour).rjust(2, "0")
-                path_in = os.path.join(path_in_day, hour_str, "output_bin.npy")
-                print "Processing {}".format(path_in)
-                with open(path_in, "r") as file_in:
-                    X = smio.load_sparse_csr(file_in)
-                for line in X:
-                    if count < num:
-                        res.append(csr_matrix(line))
-                        count += 1
-                    else:
-                        rand = random.randint(0, count)
-                        if rand < num:
-                            res[rand] = csr_matrix(line)
-            except:
-                flag = 1
-        if flag == 0:
-            path_out = os.path.join(path_in_day, "day_samp_bin.npy")
-            X = vstack(res)
-            with open(path_out, "w") as file_out:
-                smio.save_sparse_csr(file_out, X)
+def get_io_addr():
+    root = "/mnt/rips2/2016"
+
+    may = [(5, i) for i in range(1,8)]
+    june = [(6, i) for i in range(4,26)]
+    list_dates = may + june
+
+    list_io_addr = []
+    for date in list_dates:
+        month = date[0]
+        day = date[1]
+        addr_in = os.path.join(root,
+                               str(month).rjust(2, "0"),
+                               str(day).rjust(2, "0"))
+        list_io_addr.append(addr_in)
+    return list_io_addr
+
+
+def crawl(addr_day):
+    res = []
+    count = 0
+    for hour in range(0,24):
+        hour_str = str(hour).rjust(2, "0")
+        path_in = os.path.join(addr_day, hour_str, "output_bin.npy")
+        print "Processing {}".format(path_in)
+        with open(path_in, "r") as file_in:
+            X = smio.load_sparse_csr(file_in)
+        for line in X:
+            if count < num:
+                res.append(csr_matrix(line))
+                count += 1
+            else:
+                rand = random.randint(0, count)
+                if rand < num:
+                    res[rand] = csr_matrix(line)
+
+        path_out = os.path.join(addr_day, "day_samp_bin.npy")
+        X = vstack(res)
+        with open(path_out, "w") as file_out:
+            smio.save_sparse_csr(file_out, X)
+
+
+if __name__ == '__main__':
+    p = multiprocessing.Pool(8)
+    list_io_addr = get_io_addr()
+
+    for result in p.imap(crawl, list_io_addr):
+        pass
+
+    print "Completed in {} seconds\n".format(round(time.time()-start, 2))
