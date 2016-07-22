db6244cc6ad3a15d0178c2a0df39e1fcfaa9ecc9
Shurooo <wl1191@nyu.edu>
2016-07-22 12:18:19 -0700

    Next

--- Random_Forest_Test.py
+++ Random_Forest_Test.py
@@ -104,101 +104,102 @@ def test(addr_test, clf):
     return [tn, fp, fn, tp], recall, filtered
 
 
-workbook = xlsxwriter.Workbook('/home/ubuntu/Weiyi/Report/RF_Report.xlsx')
-abnormal_format = workbook.add_format()
-abnormal_format.set_bg_color("red")
-col_recall = __HEADER.index("Recall")
-col_filtered = __HEADER.index("Filtered")
+with open('/home/ubuntu/Weiyi/Report/RF_Report.xlsx', "w") as file_out:
+    workbook = xlsxwriter.Workbook(file_out)
+    abnormal_format = workbook.add_format()
+    abnormal_format.set_bg_color("red")
+    col_recall = __HEADER.index("Recall")
+    col_filtered = __HEADER.index("Filtered")
 
-for onoff_line in __ON_OFF_LINE:
-    if onoff_line == "Online":
-        if_warm_start = True
-        init_estimators = 15
-        add_estimators = 60
-    else:
-        if_warm_start = False
-        init_estimators = 75
-        add_estimators = 0
-
-    for sampling in __SAMPLING_METHOD:
-        if sampling == "Over":
-            ratio = 0.95
+    for onoff_line in __ON_OFF_LINE:
+        if onoff_line == "Online":
+            if_warm_start = True
+            init_estimators = 15
+            add_estimators = 60
         else:
-            ratio = 0.3
-        result = ["RF", onoff_line, sampling]
+            if_warm_start = False
+            init_estimators = 75
+            add_estimators = 0
 
-        ws = workbook.add_worksheet(onoff_line+"-"+sampling)
-        row = 0
-        ws.write_row(row, 0, __HEADER)
-        row += 1
+        for sampling in __SAMPLING_METHOD:
+            if sampling == "Over":
+                ratio = 0.95
+            else:
+                ratio = 0.3
+            result = ["RF", onoff_line, sampling]
 
-        for mode in __TRAIN_TEST_MODE:
-            if mode == "Next_week":
-                row += 2
-                ws.write_row(row, 0, __HEADER)
-                row += 1
+            ws = workbook.add_worksheet(onoff_line+"-"+sampling)
+            row = 0
+            ws.write_row(row, 0, __HEADER)
+            row += 1
 
-            clf = RandomForestClassifier(n_estimators=init_estimators,
-                                         max_features=15,
-                                         min_weight_fraction_leaf=0.000025,
-                                         oob_score=True,
-                                         warm_start=if_warm_start,
-                                         n_jobs=-1,
-                                         random_state=1514,
-                                         class_weight={0:1, 1:8})
-
-            pairs_by_month = get_addr_in(mode)
-            for item in pairs_by_month:
-                train_test_pairs = item[0]
-                dates_pairs = item[1]
-                for i in range(len(train_test_pairs)):
-                    result_row = result[:]
-                    result_row.extend([dates_pairs[i][0], dates_pairs[i][1]])
-
-                    pair = train_test_pairs[i]
-                    addr_train = pair[0]
-
-                    model_loaded = False
-                    if __LOAD_MODEL:
-                        print "\n>>>>> Load Model for {}".format(addr_train)
-                        model_name = "RF" + "_" + onoff_line + "_" + sampling + "_Model.p"
-                        path_in = os.path.join(addr_train, "Random_Forest_Models", model_name)
-                        try:
-                            with open(path_in, "r") as file_in:
-                                clf = pickle.load(file_in)
-                            model_loaded = True
-                        except:
-                            print ">>>>> Error: Model cannot be loaded"
-                            model_loaded = False
-
-                    if not model_loaded:
-                        print "\n>>>>> Start Training on {}".format(addr_train)
-                        start = time.time()
-                        clf = train(addr_train, clf, sampling, add_estimators)
-                        print ">>>>> Training completed in {} seconds".format(round(time.time()-start, 2))
-
-                    addr_test = pair[1]
-                    print "\n>>>>> Start Testing on {}".format(addr_test)
-                    start = time.time()
-                    stats, recall, filtered = test(addr_test, clf)
-                    print ">>>>> Testing completed in {} seconds".format(round(time.time()-start, 2))
-
-                    result_row.extend(stats)
-                    ws.write_row(row, 0, result_row)
-
-                    if recall < 0.95:
-                        ws.write(row, col_recall, str(round(recall, 4)), abnormal_format)
-                    else:
-                        ws.write(row, col_recall, str(round(recall, 4)))
-
-                    if filtered < 0.1:
-                        ws.write(row, col_filtered, str(round(filtered, 4)), abnormal_format)
-                    else:
-                        ws.write_row(row, col_filtered, str(round(filtered, 4)))
-
+            for mode in __TRAIN_TEST_MODE:
+                if mode == "Next_week":
+                    row += 2
+                    ws.write_row(row, 0, __HEADER)
                     row += 1
 
-workbook.close()
+                clf = RandomForestClassifier(n_estimators=init_estimators,
+                                             max_features=15,
+                                             min_weight_fraction_leaf=0.000025,
+                                             oob_score=True,
+                                             warm_start=if_warm_start,
+                                             n_jobs=-1,
+                                             random_state=1514,
+                                             class_weight={0:1, 1:8})
+
+                pairs_by_month = get_addr_in(mode)
+                for item in pairs_by_month:
+                    train_test_pairs = item[0]
+                    dates_pairs = item[1]
+                    for i in range(len(train_test_pairs)):
+                        result_row = result[:]
+                        result_row.extend([dates_pairs[i][0], dates_pairs[i][1]])
+
+                        pair = train_test_pairs[i]
+                        addr_train = pair[0]
+
+                        model_loaded = False
+                        if __LOAD_MODEL:
+                            print "\n>>>>> Load Model for {}".format(addr_train)
+                            model_name = "RF" + "_" + onoff_line + "_" + sampling + "_Model.p"
+                            path_in = os.path.join(addr_train, "Random_Forest_Models", model_name)
+                            try:
+                                with open(path_in, "r") as file_in:
+                                    clf = pickle.load(file_in)
+                                model_loaded = True
+                            except:
+                                print ">>>>> Error: Model cannot be loaded"
+                                model_loaded = False
+
+                        if not model_loaded:
+                            print "\n>>>>> Start Training on {}".format(addr_train)
+                            start = time.time()
+                            clf = train(addr_train, clf, sampling, add_estimators)
+                            print ">>>>> Training completed in {} seconds".format(round(time.time()-start, 2))
+
+                        addr_test = pair[1]
+                        print "\n>>>>> Start Testing on {}".format(addr_test)
+                        start = time.time()
+                        stats, recall, filtered = test(addr_test, clf)
+                        print ">>>>> Testing completed in {} seconds".format(round(time.time()-start, 2))
+
+                        result_row.extend(stats)
+                        ws.write_row(row, 0, result_row)
+
+                        if recall < 0.95:
+                            ws.write(row, col_recall, str(round(recall, 4)), abnormal_format)
+                        else:
+                            ws.write(row, col_recall, str(round(recall, 4)))
+
+                        if filtered < 0.1:
+                            ws.write(row, col_filtered, str(round(filtered, 4)), abnormal_format)
+                        else:
+                            ws.write_row(row, col_filtered, str(round(filtered, 4)))
+
+                        row += 1
+
+    workbook.close()
 
 
 
