528458b0367d31c88c9d68eb20c05f4c868b998e
Shurooo <wl1191@nyu.edu>
2016-07-23 11:46:01 -0700

    Add Get_Data

--- Get_Data.py
+++ Get_Data.py
@@ -1,10 +1,30 @@
+import os
 import numpy as np
+from scipy import vstack
 import Sparse_Matrix_IO as smio
 
 
-# Return a numpy matrix that contain 100,000 impressions,
-# with the specified ratio of positive responses and negative responses.
+# With a given address of the day to take random sample from,
+# obtain a numpy matrix that contain 100,000 impressions,
+# with the specified ratio of positive responses and negative responses,
+# and return the feature matrix X and the corresponding response vector y
 # The ratio is given by pos/neg
 def get(addr_day, ratio):
     n = 100000
-    
+    neg = int(n / (1+ratio))
+    pos = n - neg
+
+    with open(os.path.join(addr_day, "day_samp_bin_neg.npy"), "r") as file_neg:
+        matrix_neg = smio.load_sparse_csr(file_neg)
+    matrix_neg = matrix_neg[:neg, :]
+    with open(os.path.join(addr_day, "day_samp_bin_pos.npy"), "r") as file_pos:
+        matrix_pos = smio.load_sparse_csr(file_pos)
+    matrix_pos = matrix_pos[:pos, :]
+
+    matrix = vstack((matrix_neg, matrix_pos))
+    np.random.shuffle(matrix)
+    width = np.size(matrix, 1)
+    X = matrix[:, :width-1]
+    y = matrix[:, width-1]
+
+    return X, y
