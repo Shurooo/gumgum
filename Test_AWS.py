ee88fbb361ac3187b59872a31fa9444d23e9902c
Shurooo <wl1191@nyu.edu>
2016-07-26 11:29:17 -0700

    Modify Test_AWS

--- Test_AWS.py
+++ Test_AWS.py
@@ -4,10 +4,10 @@ import multiprocessing
 
 
 def get_io_addr():
-    # may = [(5, i, j) for i in range(1, 8) for j in range(24)]
-    may = []
-    june = [(6, i, j) for i in range(4, 26) for j in range(24)]
-    # june = []
+    may = [(5, i, j) for i in range(1, 8) for j in range(1)]
+    # may = []
+    # june = [(6, i, j) for i in range(4, 26) for j in range(24)]
+    june = []
     root = "/mnt/rips/2016"
 
     list_io_addr = []
@@ -28,41 +28,39 @@ def get_io_addr():
 def crawl(addr_in):
     print "Processing {}".format(addr_in)
 
-    tmax_count = 0
-    imp_count = 0
-    bidreq_count = 0
+    small_count = 0
+    large_count = 0
     with open(addr_in, "r") as file_in:
         for line in file_in:
             entry = json.loads(line)
             auction = entry["auction"]
 
-            if auction.has_key("tmax"):
-                if auction["tmax"] <= 0:
-                    tmax_count += 1
-
             if auction.has_key("bidrequests"):
-                bidreq_list = entry["auction"]["bidrequests"]
-                for bidreq in bidreq_list:
-                    bidreq_count += 1
-                    if (not bidreq.has_key("impressions")) or (len(bidreq["impressions"]) == 0):
-                        imp_count += 1
-    return tmax_count, imp_count, bidreq_count
+                for bidreq in entry["auction"]["bidrequests"]:
+                    if bidreq.has_key("impressions"):
+                        for imp in bidreq["impressions"]:
+                            if imp.has_key("banner"):
+                                w = imp["banner"]["w"]
+                                h = imp["banner"]["h"]
+                                if (w <= 0) or (h <= 0):
+                                    small_count += 1
+                                if (w >= 500) and (h >= 200):
+                                    large_count += 1
+    return small_count, large_count
 
 if __name__ == '__main__':
     cpus = multiprocessing.cpu_count()
     p = multiprocessing.Pool(cpus)
     list_io_addr = get_io_addr()
 
-    tmax_count = 0
-    imp_count = 0
-    bidreq_count = 0
+    small_count = 0
+    large_count = 0
     for result in p.imap(crawl, list_io_addr):
-        tmax_count += result[0]
-        imp_count += result[1]
-        bidreq_count += result[2]
+        small_count += result[0]
+        large_count += result[1]
 
-    print "{} auctions have tmax <= 0".format(tmax_count)
-    print "{} bid requests do not have impressions, out of {} bid requests".format(imp_count, bidreq_count)
+    print "{} banners with width or heigh <= 0".format(small_count)
+    print "{} banners with width >= 500 and height >= 200".format(large_count)
     #
     # with open("/home/ubuntu/Weiyi/abnormal_tmax.txt", "w") as file_out:
     #     file_out.write("{} auctions have tmax <= 0".format(tmax_count))
