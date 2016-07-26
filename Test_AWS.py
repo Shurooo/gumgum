b626132b7b5a1b1f245553c9c65e7e668be0bda0
Shurooo <wl1191@nyu.edu>
2016-07-26 11:01:27 -0700

    Modify Test_AWS

--- Test_AWS.py
+++ Test_AWS.py
@@ -4,9 +4,10 @@ import multiprocessing
 
 
 def get_io_addr():
-    may = [(5, i, j) for i in range(1, 8) for j in range(24)]
-    # june = [(6, i, j) for i in range(4, 26) for j in range(24)]
-    june = []
+    # may = [(5, i, j) for i in range(1, 8) for j in range(24)]
+    may = []
+    june = [(6, i, j) for i in range(4, 26) for j in range(24)]
+    # june = []
     root = "/mnt/rips/2016"
 
     list_io_addr = []
