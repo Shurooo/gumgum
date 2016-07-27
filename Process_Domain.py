968c85070d05e76b51e5fc3dea9aec2e8170088b
Shurooo <wl1191@nyu.edu>
2016-07-27 11:22:27 -0700

    Split Test_AWS into Process_Driver and Process_Domain

--- Process_Domain.py
+++ Process_Domain.py
@@ -1,35 +1,20 @@
-import os
-import csv
 import json
+import csv
 import operator
-import multiprocessing
 
 
-def get_io_addr():
+result_list = []
+
+
+def get_dates():
     may = [(5, i, j) for i in range(1, 8) for j in range(24)]
     # may = []
-    june = [(6, i, j) for i in range(4, 26) for j in range(24)]
-    # june = []
-    root = "/mnt/rips/2016"
-
-    list_io_addr = []
-    for item in may+june:
-        month = item[0]
-        day = item[1]
-        hour = item[2]
-        io_addr = os.path.join(root,
-                               str(month).rjust(2, "0"),
-                               str(day).rjust(2, "0"),
-                               str(hour).rjust(2, "0"),
-                               "part-00000")
-        addr_in = os.path.join(io_addr)
-        list_io_addr.append(addr_in)
-    return list_io_addr
+    # june = [(6, i, j) for i in range(4, 26) for j in range(24)]
+    june = []
+    return may+june
 
 
-def crawl(addr_in):
-    print "Processing {}".format(addr_in)
-
+def run(addr_in):
     dict_domain = {}
     with open(addr_in, "r") as file_in:
         for line in file_in:
@@ -43,16 +28,15 @@ def crawl(addr_in):
                 dict_domain[domain] += 1
             else:
                 dict_domain.update({domain:1})
-
     return dict_domain
 
-if __name__ == '__main__':
-    cpus = multiprocessing.cpu_count()
-    p = multiprocessing.Pool(cpus)
-    list_io_addr = get_io_addr()
 
+def add_result(result):
+    result_list.append(result)
+
+def get_result():
     dict_domain = {}
-    for result in p.imap(crawl, list_io_addr):
+    for result in result_list:
         for key in result:
             if dict_domain.has_key(key):
                 dict_domain[key] += result[key]
@@ -60,9 +44,8 @@ if __name__ == '__main__':
                 dict_domain.update({key:result[key]})
 
     print "{} unique domains recorded".format(len(dict_domain))
-
     sorted_domain = sorted(dict_domain.items(), key=operator.itemgetter(1), reverse=True)
-    with open("/home/ubuntu/Weiyi/domains_all.ods", "w") as file_out:
+    with open("/home/ubuntu/Weiyi/domains.ods", "w") as file_out:
         wr = csv.writer(file_out)
         for item in sorted_domain:
             wr.writerow(item)
---
+++ Process_Driver.py
@@ -0,0 +1,42 @@
+import os
+import csv
+import json
+import operator
+import multiprocessing
+import Process_Domain as process
+
+
+def get_io_addr(dates):
+
+    root = "/mnt/rips/2016"
+
+    list_io_addr = []
+    for item in dates:
+        month = item[0]
+        day = item[1]
+        hour = item[2]
+        io_addr = os.path.join(root,
+                               str(month).rjust(2, "0"),
+                               str(day).rjust(2, "0"),
+                               str(hour).rjust(2, "0"),
+                               "part-00000")
+        addr_in = os.path.join(io_addr)
+        list_io_addr.append(addr_in)
+    return list_io_addr
+
+
+def crawl(addr_in):
+    print "Processing {}".format(addr_in)
+    return process.run(addr_in)
+
+if __name__ == '__main__':
+    cpus = multiprocessing.cpu_count()
+    p = multiprocessing.Pool(cpus)
+    list_io_addr = get_io_addr(process.get_dates())
+
+    for result in p.imap(crawl, list_io_addr):
+        process.add_result(result)
+
+    process.get_result()
+
+
