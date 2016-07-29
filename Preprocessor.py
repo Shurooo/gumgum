7765d1d0de090830b75856adfd0cbe4480ae9ccb
Shurooo <wl1191@nyu.edu>
2016-07-29 15:47:53 -0700

    Next

--- Preprocessor.py
+++ Preprocessor.py
@@ -17,8 +17,8 @@ def get_io_addr_day_samp():
     # june = []
 
     root = "/mnt/rips2/2016"
-    filename_in = "day_samp_raw"
-    filename_out = "day_samp_new.npy"
+    filename_in = "day_samp_raw_"
+    filename_out = "day_samp_new_"
 
     list_io_addr = []
     for item in may+june:
@@ -51,34 +51,34 @@ def get_io_addr_random_sample():
 def crawl(io_addr):
     dumped = 0
     data_sparse_list = []
-    # for suffix in ["pos"]:
-    addr_in = io_addr[0]
-    addr_out = io_addr[1]
-    # addr_in = addr_in + suffix
-    # addr_out = addr_out + suffix + ".npy"
-    if os.path.isfile(addr_in):
-        with open(addr_in, "r") as file_in:
-            print "Processing {}".format(addr_in)
-            for line in file_in:
-                # try:
-                entry = json.loads(line)
-                result = []
-                Driver.process(entry, result)
-                data_sparse_list.append(csr_matrix(result))
+    for suffix in ["neg"]:
+        addr_in = io_addr[0]
+        addr_out = io_addr[1]
+        addr_in = addr_in + suffix
+        addr_out = addr_out + suffix + ".npy"
+        if os.path.isfile(addr_in):
+            with open(addr_in, "r") as file_in:
+                print "Processing {}".format(addr_in)
+                for line in file_in:
+                    # try:
+                    entry = json.loads(line)
+                    result = []
+                    Driver.process(entry, result)
+                    data_sparse_list.append(csr_matrix(result))
 
-                # except:
-                #     dumped += 1
+                    # except:
+                    #     dumped += 1
 
-        data_matrix = vstack(data_sparse_list)
-        with open(addr_out, 'w') as file_out:
-            np.savez(file_out,
-                     data=data_matrix.data,
-                     indices=data_matrix.indices,
-                     indptr=data_matrix.indptr,
-                     shape=data_matrix.shape)
+            data_matrix = vstack(data_sparse_list)
+            with open(addr_out, 'w') as file_out:
+                np.savez(file_out,
+                         data=data_matrix.data,
+                         indices=data_matrix.indices,
+                         indptr=data_matrix.indptr,
+                         shape=data_matrix.shape)
 
-    else:
-        print "\nFile Missing: {}\n".format(addr_in)
+        else:
+            print "\nFile Missing: {}\n".format(addr_in)
 
     return dumped
 
