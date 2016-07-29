5b355e7c44249ddcde9985ce9fb685be6d7b93ef
Shurooo <wl1191@nyu.edu>
2016-07-28 20:43:13 -0700

    Next

--- RF_Driver.py
+++ RF_Driver.py
@@ -10,7 +10,6 @@ max_features="sqrt"
 clf = rfw.RandomForsetWrapper(init_estimators,
                               add_estimators,
                               min_weight_fraction_leaf=min_weight_fraction_leaf,
-                              class_weight=class_weight,
                               max_features=max_features)
 
 clf.run_predict([(5, 1)], [(5, 2)])
