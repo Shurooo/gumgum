7203d1e0004a009eb0f034c3f4bfb077d974af9a
Shurooo <wl1191@nyu.edu>
2016-07-29 08:59:43 -0700

    Next

--- RF_Driver.py
+++ RF_Driver.py
@@ -1,14 +1,10 @@
 import RF_Wrapper as rfw
 
 
-init_estimators = 40
-add_estimators = 0
-min_weight_fraction_leaf=0.00000001,
-class_weight={0:1, 1:1000},
-max_features="sqrt"
-
-clf = rfw.RandomForsetWrapper(init_estimators,
-                              add_estimators,
+clf = rfw.RandomForsetWrapper(init_estimators = 40,
+                              add_estimators = 0,
+                              max_features="sqrt",
+                              min_weight_fraction_leaf=0.00000001,
                               class_weight={0:1, 1:100})
 
 clf.run_predict([(5, 1)], [(5, 2)])
