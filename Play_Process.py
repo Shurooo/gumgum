f3d9703ac6318cbfd77d3fc425dfe35b7dfb406b
Shurooo <wl1191@nyu.edu>
2016-07-31 10:49:41 -0700

    Next

--- Play_Process.py
+++ Play_Process.py
@@ -7,7 +7,7 @@ import os
 
 
 # var_ = ["cc", "rg", "margin", "tmax", "typeid", "bti", "bidderid", "verticalid", "bidfloor", "format", "product", "cat", "pcat", "domain", "bkc", "t", "w", "h"]
-var_ = ["rg", "h"]
+var_ = ["margin", "h"]
 
 
 def get_io_addr():
