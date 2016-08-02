f93325acbe8ad97d0ca19fb9dd3d1211efd6b925
Shurooo <wl1191@nyu.edu>
2016-08-02 13:53:34 -0700

    Next

--- Hour_Sampler.py
+++ Hour_Sampler.py
@@ -1,6 +1,7 @@
 import os
 import time
 import multiprocessing
+from random import shuffle
 import numpy as np
 
 
@@ -9,7 +10,7 @@ start = time.time()
 
 
 def get_io_addr():
-    may = [(5, i, j) for i in range(1, 8) for j in range(24)]
+    may = [(5, i, j) for i in range(1, 2) for j in range(0)]
     # may = []
     # june = [(6, i) for i in range(4, 26)]
     june = []
@@ -42,26 +43,23 @@ def crawl(addr_in):
             for line in file_in:
                 line_count += 1
             total_line += line_count
-    line_indices = sorted(np.random.choice(total_line, num, replace=False))
+    with open(list_path_in[0]) as file_in:
+        data = list(file_in)
+    with open(list_path_in[1]) as file_in:
+        data.extend(list(file_in))
 
-    setoff = 0
-    index = 0
-    res = []
-    for path_in in list_path_in:
-        with open(path_in, "r") as file_in:
-            for line in file_in:
-                if line_indices[index]-setoff == 0:
-                    res.append(line)
-                    index += 1
-                setoff += 1
-                if index >= num:
-                    break
-        if index >= num:
-            break
+    line_to_discard = total_line-num
+    if line_to_discard > 0:
+        line_indices = sorted(np.random.choice(line_to_discard, total_line, replace=False))
+        setoff = 0
+        for index in line_indices:
+            del data[index-setoff]
+            setoff += 1
 
+    shuffle(data)
     path_out = os.path.join(addr_in, "output_raw")
     with open(path_out, "w") as file_out:
-        for line in res:
+        for line in shuffle:
             file_out.write(line)
 
 
