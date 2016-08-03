8dac463c40e20899a9b511c1e4467ac408add804
Shurooo <wl1191@nyu.edu>
2016-08-02 17:17:30 -0700

    Next

--- RF_Driver.py
+++ RF_Driver.py
@@ -1,10 +1,11 @@
 import RF_Wrapper as rfw
 
 
-clf = rfw.RandomForsetWrapper(init_estimators=400,
-                              add_estimators=0,
+clf = rfw.RandomForsetWrapper(init_estimators=40,
+                              add_estimators=20,
+                              class_weight={0:1, 1:8.5},
+                              min_weight_fraction_leaf=0.00001,
                               max_features="sqrt")
 
-
-clf.run_predict(data_train=[(6, 4)], data_test=[(6, 5)], sampling_ratio=2.6, sampling_mode="normal")
-# clf.run_test([[(6, i) for i in range(4, 11)]], sampling_ratio=2.6, sampling_mode="normal", report_name="RF_Test.xlsx")
+# clf.run_predict(data_train=[(6, 19)], data_test=[(6, 20)], sampling_ratio=1.4, sampling_mode="normal")
+clf.run_test([[(6, i) for i in range(4, 11)]], sampling_ratio=1.4, report_name="RF_Test.xlsx")
