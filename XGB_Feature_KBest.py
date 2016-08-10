c458f940efc42e3f013f19f2eb6b9d94a31e7388
Shurooo <wl1191@nyu.edu>
2016-08-10 10:44:11 -0700

    Next

--- XGB_Feature_KBest.py
+++ XGB_Feature_KBest.py
@@ -30,7 +30,7 @@ def get_data(month, day, hour=-1):
         addr_in = os.path.join(root,
                                str(month).rjust(2, "0"),
                                str(day).rjust(2, "0"),
-                               "day_samp_new.npy")
+                               "day_samp_newer.npy")
     else:
         addr_in = os.path.join(root,
                                str(month).rjust(2, "0"),
@@ -64,11 +64,12 @@ def search_cut(prob):
     return score, recall_best, filter_rate_best, cut_best, net_savings_best
 
 
+eta = 0.05
 param = {'booster':'gbtree',   # Tree, not linear regression
          'objective':'binary:logistic',   # Output probabilities
          'eval_metric':['auc'],
          'bst:max_depth':5,   # Max depth of tree
-         'bst:eta':.1,   # Learning rate (usually 0.01-0.2)
+         'bst:eta':0.05,   # Learning rate (usually 0.01-0.2)
          'bst:gamma':0,   # Larger value --> more conservative
          'bst:min_child_weight':1,
          'scale_pos_weight':30,   # Often num_neg/num_pos
@@ -77,43 +78,41 @@ param = {'booster':'gbtree',   # Tree, not linear regression
          'save_period':0,   # Only saves last model
          'nthread':6,   # Number of cores used; otherwise, auto-detect
          'seed':25}
-num_round = 250   # Number of rounds of training, increasing this increases the range of output values
+num_round = int(250*(0.2/float(eta)))   # Number of rounds of training, increasing this increases the range of output values
 
-data = (6, 7)
+data = (6, 4)
 result_all = []
 
 X_train, y_train = get_data(data[0], data[1])
 X_test, y_test = get_data(data[0], data[1]+1)
 
-selectK = SelectKBest(f_classif, k=1000)
+selectK = SelectKBest(f_classif, k="all")
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     selectK.fit(X_train, y_train)
 
-print list(selectK.get_support(indices=True))
+for k in range(100, 2001, 100) + [2058]:
+    print "k = ", k
+    sys.stdout.flush()
+    selectK.k = k
 
-# for k in range(100, 2501, 100):
-#     print "k = ", k
-#     sys.stdout.flush()
-#     selectK.k = k
-#
-#     X_train_Sel = selectK.transform(X_train)
-#     data_train = xgb.DMatrix(X_train_Sel, label=y_train)
-#
-#     start = time.time()
-#     bst = xgb.train(param, data_train, num_round, verbose_eval=0)
-#     train_time = round(time.time() - start, 2)
-#
-#     X_test_Sel = selectK.transform(X_test)
-#     data_test = xgb.DMatrix(X_test_Sel, label=y_test)
-#
-#     start = time.time()
-#     prob = bst.predict(data_test)
-#     test_time = round(time.time() - start, 2)
-#
-#     score, recall, filter_rate, cut, net_savings = search_cut(prob)
-#     result_all.append([k, train_time, test_time, score, recall, filter_rate, cut, net_savings])
-#
-# result = pd.DataFrame(np.array(result_all), columns=["k", "train time", "test time", "score", "recall", "filter rate", "cut", "net savings"])
-# file_out_name = "/home/wlu/Desktop/Feature_Selection/KBest/KBest_{}{}.csv".format(str(data[0]).rjust(2, "0"), str(data[1]).rjust(2, "0"))
-# result.to_csv(file_out_name)
+    X_train_Sel = selectK.transform(X_train)
+    data_train = xgb.DMatrix(X_train_Sel, label=y_train)
+
+    start = time.time()
+    bst = xgb.train(param, data_train, num_round)
+    train_time = round(time.time() - start, 2)
+
+    X_test_Sel = selectK.transform(X_test)
+    data_test = xgb.DMatrix(X_test_Sel, label=y_test)
+
+    start = time.time()
+    prob = bst.predict(data_test)
+    test_time = round(time.time() - start, 2)
+
+    score, recall, filter_rate, cut, net_savings = search_cut(prob)
+    result_all.append([k, train_time, test_time, score, recall, filter_rate, cut, net_savings])
+
+result = pd.DataFrame(np.array(result_all), columns=["k", "train time", "test time", "score", "recall", "filter rate", "cut", "net savings"])
+file_out_name = "/home/wlu/Desktop/Feature_Selection/KBest/KBest_{}{}.csv".format(str(data[0]).rjust(2, "0"), str(data[1]).rjust(2, "0"))
+result.to_csv(file_out_name)
